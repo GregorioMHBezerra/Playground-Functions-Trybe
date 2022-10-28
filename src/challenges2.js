4808e73cea897c8ad2a92c5916dd61048dd4231a
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-10-28 19:24:12 -0300

    Adiciona desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,4 @@
-// Desafio 11 - colocar um array de exatos 11 numeros (ou retornar erro), que não podem se repetir mais que duas vezes (ou retornar erro), serem maiores que 9(ou retornar erro) ou menores que 0(ou retornar erro). Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], a função generatePhoneNumber deverá retornar (12) 34567-8901.
+// Desafio 11
 function generatePhoneNumber(arrayDeNumeros) {
   if (arrayDeNumeros.length !== 11) {
     return 'Array com tamanho incorreto.';
@@ -31,12 +31,15 @@ function triangleCheck(lineA, lineB, lineC) {
   }
 }
 
-// Desafio 13 -// String recebida:
-  // '1 cachaça, 5 cervejas e 1 copo de vinho'
-  // // String retornada:
-  //   '7 copos de água' - O número na frente de cada bebida deve estar entre 1 e 9. - var result=text.replace(/[^0-9]/g, ''); 
-function hydrate() {
-  // seu código aqui
+// Desafio 13
+function hydrate(string) {
+  const soNumeros = (string.replace(/[^0-9]/g, '')).split('');
+  let soma = 0;
+  for (const key in soNumeros) {
+    soma += parseInt(soNumeros[key]);
+  }
+  
+  return (soma === 1) ? `${soma} copo de água` : `${soma} copos de água`;
 }
 
 module.exports = {
